780767f76db1364486793e0924d62e3ea1a6b84b
gungunawan31 <gunawanku1931@gmail.com>
2020-04-26 12:04:05 +0700

    repair bug for client

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('id');
+            $table->string('id',100);
             $table->primary('id');
             $table->string('username')->nullable();
             $table->string('nama_lenkap')->nullable();
--- database/migrations/2020_04_10_125811_create_table_time.php
+++ database/migrations/2020_04_10_125811_create_table_time.php
@@ -14,9 +14,9 @@ class CreateTableTime extends Migration
     public function up()
     {
         Schema::create('table_time', function (Blueprint $table) {
-            $table->string('id_time')->nullable();
+            $table->string('id_time',100)->nullable();
             $table->primary('id_time');
-            $table->string('time')->nullable()->unique();
+            $table->string('time',100)->nullable()->unique();
             $table->string('status_time_service')->nullable();
             $table->string('created_by')->nullable();
             $table->timestamps();
